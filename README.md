d1f4e04c2ddade4ae81ab9104186582c14b68cf2
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2013-09-11 06:40:10 +0900

    add S2Robot.

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ Elasticsearch River Reb
 
 ## Usage
 
+    $ curl -XDELETE 'localhost:9200/_river/my_web/'
+
     $ curl -XPUT 'localhost:9200/_river/my_web/_meta' -d '{
         "type" : "web",
+        "crawl" : {
+            "url" : ["http://www.codelibs.org", "http://fess.codelibs.org/"],
+            "maxDepth" : 1
+        },
         "schedule" : {
             "cron" : "0 * * * * ?"
         }
--- src/main/java/org/codelibs/elasticsearch/web/river/WebRiver.java
+++ src/main/java/org/codelibs/elasticsearch/web/river/WebRiver.java
@@ -4,6 +4,7 @@ import static org.quartz.CronScheduleBuilder.cronSchedule;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
 
+import java.util.List;
 import java.util.Map;
 
 import org.codelibs.elasticsearch.web.service.S2ContainerService;
@@ -22,6 +23,11 @@ import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.Trigger;
+import org.seasar.framework.container.S2Container;
+import org.seasar.robot.S2Robot;
+import org.seasar.robot.entity.AccessResult;
+import org.seasar.robot.service.DataService;
+import org.seasar.robot.util.AccessResultCallback;
 
 public class WebRiver extends AbstractRiverComponent implements River {
     private static final String S2_CONTAINER = "s2Container";
@@ -62,6 +68,7 @@ public class WebRiver extends AbstractRiverComponent implements River {
         logger.info("Scheduling CrawlJob...");
 
         final JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put("settings", settings);
         jobDataMap.put(ES_CLIENT, client);
         jobDataMap.put(S2_CONTAINER, containerService.getContainer());
         final JobDetail crawlJob = newJob(CrawlJob.class)
@@ -101,9 +108,58 @@ public class WebRiver extends AbstractRiverComponent implements River {
                 throws JobExecutionException {
 
             final JobDataMap data = context.getMergedJobDataMap();
-            logger.info("client: " + data.get(ES_CLIENT));
-            logger.info("container: " + data.get(S2_CONTAINER));
+            Client client = (Client) data.get(ES_CLIENT);
+            S2Container container = (S2Container) data.get(S2_CONTAINER);
+            RiverSettings settings = (RiverSettings) data.get("settings");
+            @SuppressWarnings("unchecked")
+            Map<String, Object> crawlSettings = (Map<String, Object>) settings
+                    .settings().get("crawl");
+            if (crawlSettings == null) {
+                logger.warn("No settings for crawling.");
+                return;
+            }
+
+            S2Robot s2Robot = (S2Robot) container.getComponent(S2Robot.class);
+            // add url
+            @SuppressWarnings("unchecked")
+            List<String> urlList = (List<String>) crawlSettings.get("url");
+            if (urlList == null || urlList.isEmpty()) {
+                logger.warn("No url for crawling.");
+                return;
+            }
+            for (String url : urlList) {
+                s2Robot.addUrl(url);
+            }
+            // depth
+            int maxDepth = getInitParameter(crawlSettings, "maxDepth", -1);
+            s2Robot.getRobotContext().setMaxDepth(maxDepth);
+
+            // run s2robot
+            String sessionId = s2Robot.execute();
+
+            // print urls
+            DataService dataService = (DataService) container
+                    .getComponent(DataService.class);
+            logger.info("Crawled URLs: ");
+            dataService.iterate(sessionId, new AccessResultCallback() {
+                public void iterate(AccessResult accessResult) {
+                    logger.info(accessResult.getUrl());
+                }
+            });
+
+            // clean up
+            // s2Robot.cleanup(sessionId);
+
         }
 
+        @SuppressWarnings("unchecked")
+        private <T> T getInitParameter(Map<String, Object> crawlSettings,
+                String key, T defaultValue) {
+            Object value = crawlSettings.get(key);
+            if (value != null) {
+                return (T) value;
+            }
+            return defaultValue;
+        }
     }
 }
