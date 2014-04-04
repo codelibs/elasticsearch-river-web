1bfd704670e0e9947898d6dd1e95ac02a691e6f7
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-04-04 09:19:37 +0900

    update version

--- src/main/java/org/codelibs/elasticsearch/web/WebRiverConstants.java
+++ src/main/java/org/codelibs/elasticsearch/web/WebRiverConstants.java
@@ -5,6 +5,6 @@ public final class WebRiverConstants {
     private WebRiverConstants() {
     }
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.2";
 
 }
